37884fc01edd54e5fa2c28ca79e20ee165c49adc
Luis Espinosa <82414418+LuisJose28@users.noreply.github.com>
2021-11-29 11:59:11 -0500

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,6 +1,7 @@
+
 #include <stdio.h>
 
-void AsientosCine() 
+void AsientosCine() // esta funcion imprime la distribucion de los asientos del cine
 {
     int cine[8][8];
     int x, y;
@@ -25,18 +26,7 @@ void AsientosCine()
     return;
 }
 
-void PreguntarACliente()//Funcion que pregunta al cliente que silla desea
-{
-    int silla1, silla2;
 
-    printf("\nSeleccione la Fila que desea: ");
-    scanf("%i", &silla1);
-
-    printf("\nSeleccione la Columna que desea: ");
-    scanf("%i", &silla2);
-
-    return;
-}
 
 void SillasReservadas() //Funcion que impre las sillas que han sido reservadas
 {
@@ -49,12 +39,6 @@ void SillasReservadas() //Funcion que impre las sillas que han sido reservadas
 
     printf("\nSeleccione la Columna que desea: ");
     scanf("%i", &silla2);
-
-    if (silla1 == 1)
-    {
-        /* code */
-    }
-    
 
     for (x = 0; x < 8; x++)
     {
@@ -87,15 +71,11 @@ int main(){
     printf("\n-Reservacion de Asientos \n");
     AsientosCine();// funciones que imprime la reservacion de asientos
 
-    SillasReservadas();
     
 
     for (b = 0; b < 32; b++)
     {
-        
-        
         SillasReservadas();
-
     }
     
 
