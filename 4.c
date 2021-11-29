037647488de3eb3e73ee9250bfb7f34dbb994683
Luis Espinosa <82414418+LuisJose28@users.noreply.github.com>
2021-11-28 20:34:29 -0500

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+void AsientosCine() 
+{
+    int cine[8][8];
+    int x, y;
+
+    for (x = 0; x < 8; x++)
+    {
+        for (y = 0; y < 8; y++)
+        {
+            cine[x][y] = 11;
+        }
+    }
+    
+    for (x = 0; x < 8; x++)
+    {
+        for (y = 0; y < 8; y++)
+        {
+            printf("|%d", cine[x][y]);
+        }
+        printf("\n");
+    }
+
+    return;
+}
+
+void PreguntarACliente()//Funcion que pregunta al cliente que silla desea
+{
+    int silla1, silla2;
+
+    printf("\nSeleccione la Fila que desea: ");
+    scanf("%i", &silla1);
+
+    printf("\nSeleccione la Columna que desea: ");
+    scanf("%i", &silla2);
+
+    return;
+}
+
+void SillasReservadas() //Funcion que impre las sillas que han sido reservadas
+{
+    int cine[8][8];
+    int x, y;
+    int silla1, silla2;
+
+    for (x = 0; x < 8; x++)
+    {
+        for (y = 0; y < 8; y++)
+        {
+            cine[silla1][silla2] = 88;
+        }
+    }
+
+    for (x = 0; x < 8; x++)
+    {
+        for (y = 0; y < 8; y++)
+        {
+            printf("|%d", cine[x][y]);
+        }
+            printf("\n");
+    }
+    return;
+}
+
+
+int main(){
+
+    int cine[8][8];
+    int x, y;
+    int terminar;
+
+    int b; //esta variable sera el maximo de capacidad para reservar una silla
+
+    printf("\n-Reservacion de Asientos \n");
+    AsientosCine();// funciones que imprime la reservacion de asientos
+
+      return 0; 
+}
